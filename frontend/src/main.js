a1d8913c5298dd91f5b414d50c53cff1bd364e5b
zjk <1183209167@qq.com>
2026-05-19 21:18:00 +0800

    上传图片测试结果完成

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js' // 引⼊路由
 
+// ======= 🎯 核心：补上 Element Plus 的 JS 和 CSS 样式 =======
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 
-app.use(router) // 启⽤路由
+// 启⽤路由
+app.use(router)
+
+// ======= 🎯 核心：注册启用 Element Plus =======
+app.use(ElementPlus)
+
 app.mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import index from "../views/index.vue"; // 你的检测页面
 import Detection from "../views/Detection.vue";
+import inference from "../views/Inference.vue"; // Yolo 推理验证⻚⾯
+// 路由配置
 
 const routes = [
   {
@@ -13,6 +15,11 @@ const routes = [
     path: "/detection",
     name: "detection",
     component: Detection, // 默认打开这个带切换模式的页面
+  },
+  {
+    path: "/inference",
+    name: "inference",
+    component: inference, // 默认打开就是检测⻚⾯
   },
 ];
 
